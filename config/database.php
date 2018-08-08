3bf42256757aba86c3704fc18e16c165cde128e1
shenyan <52o@qq52o.cn>
2018-08-08 10:23:10 +0800

    数据库配置

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => \Env::get('database.hostname'),
     // 数据库名
-    'database'        => '',
+    'database'        => \Env::get('database.database'),
     // 用户名
-    'username'        => 'root',
+    'username'        => \Env::get('database.username'),
     // 密码
-    'password'        => '',
+    'password'        =>  \Env::get('database.password'),
     // 端口
     'hostport'        => '',
     // 连接dsn
@@ -29,7 +29,7 @@ return [
     // 数据库编码默认采用utf8
     'charset'         => 'utf8',
     // 数据库表前缀
-    'prefix'          => '',
+    'prefix'          => \Env::get('database.prefix'),
     // 数据库调试模式
     'debug'           => true,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
