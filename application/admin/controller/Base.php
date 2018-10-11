8a583c74d1ea91af3b0adacf979da6c981247e8c
fphd_xian_lufei <52o@qq52o.cn>
2018-10-11 19:32:25 +0800

    登录验证

--- application/admin/controller/Base.php
+++ application/admin/controller/Base.php
@@ -11,5 +11,12 @@ use think\Controller;
 
 class Base extends Controller
 {
-
+    protected function initialize()
+    {
+        // 判断用户是否登录
+        $uid = session('user_id');
+        if (!isset($uid)) {
+            return $this->error('对不起,您还没有登录!请先登录', '/admin/User/login');
+        }
+    }
 }
