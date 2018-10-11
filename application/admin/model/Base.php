f3703d690edb7609bd80afb5cd615029aa9efc6f
fphd_xian_lufei <52o@qq52o.cn>
2018-10-11 18:55:24 +0800

    公用model

---
+++ application/admin/model/Base.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ShenYan
+ * Date: 2018/10/11
+ * Time: 下午6:47
+ */
+
+namespace app\admin\model;
+
+use think\Model;
+
+class Base extends Model
+{
+
+}
