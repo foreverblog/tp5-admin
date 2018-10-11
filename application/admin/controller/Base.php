7c62b38fc911b05517b36bffc4d20d7d908b6483
fphd_xian_lufei <52o@qq52o.cn>
2018-10-11 17:56:02 +0800

    后台

---
+++ application/admin/controller/Base.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ShenYan
+ * Date: 2018/10/11
+ * Time: 下午1:46
+ */
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Base extends Controller
+{
+
+}
---
+++ application/admin/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ShenYan
+ * Date: 2018/10/11
+ * Time: 下午1:45
+ */
+namespace app\admin\controller;
+
+class Index extends Base
+{
+    public function index()
+    {
+        return 'index';
+    }
+}
