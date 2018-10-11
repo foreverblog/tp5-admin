de041f96db9aa074662b75b7c35a98a13a387967
fphd_xian_lufei <52o@qq52o.cn>
2018-10-11 18:18:13 +0800

    后台管理登录

---
+++ application/admin/controller/User.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ShenYan
+ * Date: 2018/10/11
+ * Time: 下午6:15
+ */
+namespace app\admin\controller;
+
+use think\Controller;
+
+class User extends Controller
+{
+    public function login()
+    {
+
+    }
+}
