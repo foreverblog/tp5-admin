9ab005bbfd43ffdd0d785bb1066a36d204ef2308
shenyan <52o@qq52o.cn>
2018-10-13 11:59:24 +0800

    修改配置

--- config/app.php
+++ config/app.php
@@ -131,7 +131,8 @@ return [
 
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
-    'dispatch_error_tmpl'    => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
+//    'dispatch_error_tmpl'    => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
+    'dispatch_error_tmpl'    => 'static/tpl/dispatch_jump.tpl',
 
     // 异常页面的模板文件
     'exception_tmpl'         => Env::get('think_path') . 'tpl/think_exception.tpl',
