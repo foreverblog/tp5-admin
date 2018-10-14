a55af70f167c0c9fe1720b59978ddd990c52ae3e
shenyan <52o@qq52o.cn>
2018-10-14 11:29:37 +0800

    增加邮箱配置

---
+++ config/extra.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 自定义配置文件
+ * author: ShenYan.
+ * Email：52o@qq52o.cn
+ * CreatedTime: 2018/10/13 20:59
+ */
+return [
+    // 配置邮件发送服务器
+    'smtp_debug'                    => false,
+    'smtp_host'                     => 'smtp.exmail.qq.com',
+    'smtp_username'                 => 'notice@foreverblog.cn',//SMTP服务器登陆用户名
+    'smtp_password'                 => '',//SMTP服务器登陆密码
+    'smtp_secure'                   => 'ssl', //tls 端口25 ssl465
+    'smtp_port'                     => '465', //tls 端口25 ssl465
+];
