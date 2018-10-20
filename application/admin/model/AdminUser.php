e6df6a082b0a4c23e854cbb3edd498872cc50936
沈唁 <52o@qq52o.cn>
2018-10-20 11:48:42 +0800

    Update AdminUser.php

--- application/admin/model/AdminUser.php
+++ application/admin/model/AdminUser.php
@@ -8,6 +8,7 @@
 
 namespace app\admin\model;
 
+use mcrypt\Mcrypt;
 
 class AdminUser extends Base
 {
@@ -15,7 +16,8 @@ class AdminUser extends Base
     protected $pk = 'id';
 
     // 自动完成
-    protected $auto = ['now_ip', 'login_num', 'now_time'];
+    protected $update = ['now_ip', 'login_num', 'now_time'];
+    protected $insert = ['login_num' => 0,'status' => 1, 'level_id' => 3, 'nick_name' => '请修改昵称'];
 
     protected function setLoginNumAttr($value)
     {
@@ -24,6 +26,7 @@ class AdminUser extends Base
 
     protected function setNowIpAttr()
     {
+
         return request()->ip();
     }
 
@@ -40,6 +43,8 @@ class AdminUser extends Base
             return false;
         } elseif ($user['password'] != $data['password']) {
             return false;
+        } elseif ($user['status'] != 0) {
+            return 1;
         }
         $user->last_ip = $user['now_ip'];
         $user->last_time = $user['now_time'];
@@ -47,5 +52,61 @@ class AdminUser extends Base
         session('user', json_decode($user, true));
         session('user_id', $user['id']);
         return $user;
+    }
+
+    // 邮箱注册
+    public function registerByEmail($data)
+    {
+        //检查用户名是否可用
+        $user = AdminUser::where('user_name', $data['userName'])->find();
+        if ($user) {
+            return ['code' => 1, 'msg' => '用户名重复，请重新输入'];
+        }
+        //检查邮箱是否重复
+        $user = AdminUser::where('user_email', $data['email'])->find();
+        if ($user) {
+            return ['code' => 1, 'msg' => '邮箱重复，请联系管理员'];
+        }
+        $user = new AdminUser;
+        $user->user_name = $data['userName'];
+        $user->user_email = $data['email'];
+        $user->password = $data['password'];
+        $user->create_at = time();
+        $user->save();
+
+        $activeCode = Mcrypt::encode($data['userName']);//生成激活码
+        $email = $data['email'];
+
+        //发送邮件
+        $link = config('extra.web_domain') . url('activation') . '?user_id=' . $user->id . '&active_code=' . $activeCode;
+        sendMail($email, '十年之约用户激活邮件', "欢迎注册十年之约。<br/>请点击下面的链接进行激活：<a target='_blank' href=" . $link . ">" . $link . "</a>");
+
+        return ['code' => 0, 'msg' => '激活邮件已发送，请查看邮箱', 'data' => $user];
+    }
+
+    // 激活验证
+    public function activation($userId, $activeCode)
+    {
+        //检查用户是否存在
+        $user = AdminUser::get($userId);
+        if (empty($user)) {
+            return false;
+        }
+        $userName = $user->user_name;
+        $userNameCode = Mcrypt::decode($activeCode);
+        if ($userName != $userNameCode) {
+            return false;
+        }
+
+        $user->status    = 0;
+        $status = $user->save();
+
+        session('user', json_decode($user, true));
+        session('user_id', $user['id']);
+
+        if (!empty($status)) {
+            return true;
+        }
+
     }
 }
