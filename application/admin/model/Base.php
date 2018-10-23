560339c99469a74956b28e7026926429666bc2b1
沈唁 <52o@qq52o.cn>
2018-10-23 15:44:11 +0800

    Update Base.php

--- application/admin/model/Base.php
+++ application/admin/model/Base.php
@@ -12,5 +12,16 @@ use think\Model;
 
 class Base extends Model
 {
+    // 获取器 对模型实例的（原始）数据做出自动处理
+    public function getCreateAtAttr($value)
+    {
+        $time = date('Y-m-d H:i:s', $value);
+        return $time;
+    }
 
+    public function getLevelIdAttr($value)
+    {
+        $status = [1=>'超级管理员',2=>'管理员',3=>'审核员'];
+        return $status[$value];
+    }
 }
